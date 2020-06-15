afc2893dbd14de3068581e19ff3f472fc90c6f3c
TylerAStephens <tylerstephens16@yahoo.com>
2020-06-15 17:18:14 -0400

    start game

--- game.js
+++ game.js
@@ -3,17 +3,38 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
+// Set game state before playing
+var started = false;
+var level = 0;
+
+// Start Game with keypress
+$(document).keypress(function () {
+  if (!started) {
+    //Change title once game starts
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 
 $(".btn").click(function() {
   // Find selected id
   var userChosenColour = $(this).attr("id");
   //Add selected button id to array
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
 });
 
 
 function nextSequence() {
+  // Update level
+  level++;
+  $("#level-title").text("Level " + level);
+
   // Random Number Generator
   var randomNumber = Math.floor(Math.random() * 4);
   console.log(randomNumber);
@@ -24,6 +45,21 @@ function nextSequence() {
   // Animation
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
   // Audio
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour);
+
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+function animatePress(currentColour) {
+  // Add animation
+  $("#" + currentColour).addClass("pressed");
+  // Remove animation
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+
 }
